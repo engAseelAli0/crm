46a773ac937c13f6af9340e0262900b8111c67a3
engAseelAli0 <engaseelalialhamly@gmail.com>
2026-02-02 23:15:20 +0300

    fix suprbase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const originalUrl = 'https://fjeudnouuemtpfprubju.supabase.co';
 
 const supabaseUrl = import.meta.env.PROD 
-  ? '/api/supabase' 
+  ? `${window.location.origin}/api/supabase` 
   : originalUrl;
 
 const supabaseKey = 'sb_publishable_GkASHcsoGb8dA5tHR2TAVg_U3Mz34q1';
